2a92d5cef3d9723c36438987cf0aa61be2509281
chenzhuo18 <chenzhuo18@meituan.com>
2021-02-15 13:08:20 +0800

    fix: 2048 pwa

--- 2048/vite.config.js
+++ 2048/vite.config.js
@@ -1,6 +1,27 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default {
   base: '/2048/dist/',
   build: {
     assetsInlineLimit: 1024
   },
+  plugins: [
+    VitePWA({
+      manifest: {
+        "name": "2048",
+        "short_name": "2048",
+        "display": "standalone",
+        "start_url": "index.html",
+        "theme_color": "#F1E7DF",
+        "background_color": "#F1E7DF",
+        "icons": [
+          {
+            "src": "/2048/assets/icon.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
+  ]
 }
